59871cea8cb04f9ab5c36327411ded2ea0e2bd95
elogrus <elogrus@gmail.com>
2025-09-08 17:02:31 +0500

    +About Landing page (with adaptive)

--- src/Main/MyRouter.tsx
+++ src/Main/MyRouter.tsx
@@ -1,19 +1,12 @@
 import { Route, Routes } from "react-router";
 import { PageSkeleton } from "./PageSkeleton";
+import { About } from "@/Pages/Landings/About";
 
 export const MyRouter = () => {
     return (
         <Routes>
             <Route path="*" element={<PageSkeleton />}>
-                <Route
-                    index
-                    element={
-                        <h4>
-                            asd asda asd asd asd asd asd asd asd asd asd asd asd
-                            asd asd asd
-                        </h4>
-                    }
-                />
+                <Route path="about" element={<About />} />
             </Route>
         </Routes>
     );
