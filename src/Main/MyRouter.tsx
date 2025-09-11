325ef1794007a5077019df2f3ff9740a47926f07
elogrus <elogrus@gmail.com>
2025-09-11 17:42:44 +0500

    +Main Landing (with adaptive)

--- src/Main/MyRouter.tsx
+++ src/Main/MyRouter.tsx
@@ -1,12 +1,15 @@
 import { Route, Routes } from "react-router";
 import { PageSkeleton } from "./PageSkeleton";
 import { About } from "@/Pages/Landings/About";
+import { Main } from "@/Pages/Landings/Main";
 
 export const MyRouter = () => {
     return (
         <Routes>
             <Route path="*" element={<PageSkeleton />}>
+                <Route index element={<Main />} />
                 <Route path="about" element={<About />} />
+                <Route path="*" element={<h1>404</h1>} />
             </Route>
         </Routes>
     );
